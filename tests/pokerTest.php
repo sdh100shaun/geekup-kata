e502384630fa95eae85ad61d8686fef801821cd2
Shaun Hare <shaun@shaunhare.co.uk>
2014-02-03 19:53:21 +0000

    testing samesuit

--- tests/pokerTest.php
+++ tests/pokerTest.php
@@ -17,4 +17,39 @@ class PokerTest extends PHPUnit_Framework_TestCase
 		
 		$this->assertNotNull($this->poker);
 	}
+
+	/**
+	*@dataProvider hands
+	*
+	*/
+	public function testHandsIsArray($data)
+	{
+		
+	}
+
+	public function testSameSuitFalseWhenNotSameSuit()
+	{
+		$blackHand = array("black"=>$this->hands()["black"][0]);
+		$whiteHand = array("white"=>$this->hands()["white"][0]);
+		
+		$hands = array_merge($blackHand,$whiteHand);
+
+		
+
+		$ranking = new Ranking()
+		$this->assertFalse($ranking->areCardsAllSameSuit($hands));
+	}
+
+	public function hands()
+	{
+		return array(
+				"black"=>array(array("2H","3D","5S","9C", "KD"),  
+				         array("2H","4S","4C","2D","4H"),  
+						 array("2H","3D","5S","9C", "KD"),  
+						 array("2H","3D","5S","9C", "KD")), 
+				"white"=>array(array("2C","3H","4S","8C", "AH"),
+					    array("2S","8S", "AS", "QS","3S"),
+					    array("2C","3H","4S","8C", "KH"),
+					    array("2D","3H","5C","9S", "KH")));
+	}
 }
